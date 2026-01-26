5b50dd11fd85c32b9b5b31c02fed4d7feecf1b4a
Amit Jaggernauth <Amitj21@aol.com>
2026-01-25 19:31:04 -0500

    Add files via upload

---
+++ Excercise 2.3-Python/author.py
@@ -0,0 +1,11 @@
+"""
+Author: Your Name
+Date: January 25, 2026
+File Name: quote_program.py
+Description: This program prints a quote of my choosing
+and gives credit to the original author.
+"""
+
+# Printing a quote with attribution
+# The print function outputs text to the console
+print("“In the middle of every difficulty lies opportunity.” - Albert Einstein")
